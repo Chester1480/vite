28d4c2be6adaae8a1d1ac333f7cc52ff65d4bbc1
chester <chester0148@gmail.com>
2022-12-21 02:56:56 +0800

    add: 新增vue-router  和  pinia

--- src/main.js
+++ src/main.js
@@ -1,5 +1,17 @@
 import { createApp } from 'vue'
-import './style.css'
+import { createPinia } from 'pinia'
+import router from './router'
+import './assets/css/style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+// createApp(App).mount('#app')
+
+createApp(App).use(createPinia()).use(router).mount('#app')
+
+
+// import { createApp } from 'vue'
+// import { createPinia } from 'pinia'
+// import router from '@/router'
+// import App from "@/App.vue"
+
+
